c989d400018ec1c7b7dc2ae8dc1595e4c779ce6c
Julien Bouteloup <julien.bouteloup@net-a-porter.com>
2016-04-15 00:52:15 +0100

    Add package.js with webpack common

---
+++ config/webpack.common.js
@@ -0,0 +1,12 @@
+/**
+ * @author @bneiluj
+ */
+
+ var webpack = require('webpack');
+ var helpers = require('./helpers');
+
+/**
+ * Webpack Plugins
+ */
+ var CopyWebpackPlugin = require('core-webpack-plugin');
+ var HtmlWebpackPlugin = require('html-webpack-plugin');
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,3 +14,15 @@ var commonConfig = require('./webpack.common.js');
  /**
   * Webpack Contants
   */
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const HMR = helpers.hasProcessFlag('hot');
+const METADATA = webpackMerge(commonConfig.metadata, {
+  host: 'localhost',
+  port: 3000,
+  ENV: ENV,
+  HMR: HMR
+});
+
+/**
+ * Webpack configuration
+ */
