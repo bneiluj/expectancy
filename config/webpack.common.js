4df5c0a1621063714823f4a31301858045670d97
bneiluj <julien.bouteloup@gmail.com>
2016-04-18 18:04:56 +0100

    Add info to webpack common file

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -11,4 +11,25 @@
  var CopyWebpackPlugin = require('core-webpack-plugin');
  var HtmlWebpackPlugin = require('html-webpack-plugin');
 
- 
+/**
+ * Webpack Constants
+ */
+ const METADATA = {
+   title: 'Expectancy app',
+   baseUrl: '/'
+ };
+
+/**
+ * Webpack configuration
+ */
+
+ module.exports = {
+  //  static info for index.html
+  metada: METADATA,
+  // angular app entry point
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'main': './src/main.browser.ts'
+  }
+ }
