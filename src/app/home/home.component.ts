eed0a12ed487424e7b0cebd5c5f00e2ab4694352
bneiluj <julien.bouteloup@gmail.com>
2016-04-25 19:04:29 +0100

    Add home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,3 +3,24 @@ import {AppState} from '../app.service';
 
 import {Title} from './title';
 import {XLarge} from './x-large';
+
+@Component({
+  // The selector is what angular internally uses
+  // for `document.querySelectorAll(selector)` in our index.html
+  // where, in this case, selector is the string 'home'
+  selector: 'home',  //<home></home>
+  // Angular's Dependency Injection - providers are in our app.
+  providers: [
+    Title
+  ],
+  // Directives
+  directive: [
+    XLarge
+  ],
+  // Custom pipes in our template.
+  pipes: [ ],
+  // Styles
+  styles: [ require('./home.css') ],
+  // Every Angular template is first compiled by the browser before Angular runs it's compiler
+  template: require('./home.html')
+})
