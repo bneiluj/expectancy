2a4f049acd14354d034ad72b952d213155842893
bneiluj <julien.bouteloup@gmail.com>
2016-04-26 18:44:02 +0100

    Modify gitignore

---
+++ dist/main.bundle.js
@@ -0,0 +1,8 @@
+webpackJsonp([2],[
+/* 0 */
+/***/ function(module, exports) {
+
+	eval("\"use strict\";\r\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IjsiLCJzb3VyY2VSb290IjoiIn0=");
+
+/***/ }
+]);
---
+++ src/main.browser.ts
@@ -0,0 +1,10 @@
+/*
+ * Providers provided by Angular
+ */
+import {bootstrap} from 'angular2/platform/browser';
+
+/*
+* App Component
+* our top level component that holds all of our components
+*/
+import {App, APP_PROVIDERS} from './app';
